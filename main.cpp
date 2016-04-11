7e09a3f3beda041fa4464b0a19b2054546ff9e8d
sly-loki <you@example.com>
2016-04-11 23:06:33 +0300

    add server functional: listening for connection and sending data to client

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <QCoreApplication>
 #include <QTimer>
 #include <iostream>
+#include <sstream>
 #include <cstdio>
 #include <QFile>
 #include <QThread>
+#include <QTcpServer>
+#include <QTcpSocket>
 
 void onTick();
 
 QFile f("/dev/midi3");
+QTcpServer server;
 
 enum EventType
 {
@@ -42,6 +46,7 @@ protected:
             {
                 f.read(data, 2);
                 EventType type = (data[1])?ET_KeyDown:ET_KeyUp;
+                std::cout << "emit event";
                 emit newEvent({type, data[0]});
             }
         }
@@ -49,6 +54,7 @@ protected:
 };
 
 void event(PianoKeyEvent event);
+void onNewConnection();
 
 int main(int argc, char *argv[])
 {
@@ -58,20 +64,40 @@ int main(int argc, char *argv[])
     QObject::connect(&t, SIGNAL(finished()), &a, SLOT(quit()));
     QObject::connect(&t, &ReadingThread::newEvent, event);
     t.start();
-
+    QObject::connect(&server, &QTcpServer::newConnection, onNewConnection);
+    if (!server.listen(QHostAddress::Any, 1289))
+        std::cout << "listen error" << std::endl;
     return a.exec();
+}
+
+QTcpSocket * clientConnection = nullptr;
+
+void onNewConnection()
+{
+    std::cout << "new connection" << std::endl;
+    clientConnection = server.nextPendingConnection();
+//    std::cout << clientConnection->
+    clientConnection->write("connection accepted");
 }
 
 void event(PianoKeyEvent event)
 {
+    std::stringstream ss;
+    ss << std::hex;
     switch(event.type)
     {
     case ET_KeyDown:
-        std::cout << event.key << " down" << std::endl;
+        ss << event.key << " dw";
         break;
     case ET_KeyUp:
-        std::cout << event.key << " up" << std::endl;
+        ss << event.key << " up";
         break;
+    }
+
+    std::cout << ss.str() << std::endl;
+    if (clientConnection) {
+        clientConnection->write(ss.str().c_str());
+        clientConnection->flush();
     }
 }
 
