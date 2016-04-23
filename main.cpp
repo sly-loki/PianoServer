122d1b7a6a50b08195fb6239b990e0039f8f25d8
sly-loki <you@example.com>
2016-04-23 18:59:47 +0300

    Add time syncronisation

--- main.cpp
+++ main.cpp
@@ -7,22 +7,26 @@
 #include <QThread>
 #include <QTcpServer>
 #include <QTcpSocket>
+#include <QTime>
 
 void onTick();
 
 QFile f("/dev/midi3");
 QTcpServer server;
+QTimer timer;
 
 enum EventType
 {
     ET_KeyDown,
-    ET_KeyUp
+    ET_KeyUp,
+    ET_Time
 };
 
 struct PianoKeyEvent
 {
     EventType type;
     unsigned int key;
+    QTime time;
 };
 
 class ReadingThread : public QThread
@@ -46,8 +50,8 @@ protected:
             {
                 f.read(data, 2);
                 EventType type = (data[1])?ET_KeyDown:ET_KeyUp;
-                std::cout << "emit event";
-                emit newEvent({type, data[0]});
+//                std::cout << "emit event";
+                emit newEvent({type, data[0], QTime::currentTime()});
             }
         }
     }
@@ -55,6 +59,7 @@ protected:
 
 void event(PianoKeyEvent event);
 void onNewConnection();
+void syncTask();
 
 int main(int argc, char *argv[])
 {
@@ -67,6 +72,9 @@ int main(int argc, char *argv[])
     QObject::connect(&server, &QTcpServer::newConnection, onNewConnection);
     if (!server.listen(QHostAddress::Any, 1289))
         std::cout << "listen error" << std::endl;
+    timer.setInterval(1000);
+    QObject::connect(&timer, &QTimer::timeout, syncTask);
+    timer.start();
     return a.exec();
 }
 
@@ -80,6 +88,12 @@ void onNewConnection()
     clientConnection = server.nextPendingConnection();
 //    std::cout << clientConnection->
 //    clientConnection->write("connection accepted");
+    event({ET_Time, 0, QTime::currentTime()});
+}
+
+void syncTask()
+{
+    event({ET_Time, 0, QTime::currentTime()});
 }
 
 void event(PianoKeyEvent event)
@@ -89,12 +103,17 @@ void event(PianoKeyEvent event)
     switch(event.type)
     {
     case ET_KeyDown:
-        ss << event.key << " dw";
+        ss << event.key << " d ";
         break;
     case ET_KeyUp:
-        ss << event.key << " up";
+        ss << event.key << " u ";
         break;
+    case ET_Time:
+        ss << "00 t ";
     }
+
+    QString times = event.time.toString("HH mm ss zzz");
+    ss << times.toStdString();
 
     std::cout << ss.str() << std::endl;
     if (clientConnection) {
